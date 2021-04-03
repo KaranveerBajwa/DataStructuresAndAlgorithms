850fbfc5106260b811312a3391718eeac30168ba
Karan Bajwa <v-bajkar@microsoft.com>
2021-04-03 04:19:55 -0700

    Subsets of string

---
+++ SubsetsOfString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SubsetsOfString
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      string str = "ABCD";
+      FindSubsets(str);
+    }
+
+    static void FindSubsets(string str)
+    {
+      FindSubsets("", str);
+    }
+
+    static void FindSubsets(string soFar, string remaining)
+    {
+      if (remaining.Length == 0)
+      {
+        Console.WriteLine(soFar);
+        return;
+      }
+      FindSubsets(soFar + remaining[0], remaining.Substring(1));
+      FindSubsets(soFar, remaining.Substring(1));
+    }
+  }
+}
