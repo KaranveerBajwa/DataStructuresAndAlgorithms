88b274e0e4c66d0a230f2c0031b1cb16960a7fd4
Karan Bajwa <v-bajkar@microsoft.com>
2021-04-03 08:25:39 -0700

    Added DiceRoll permutaions for n dice

---
+++ PrintPossibleDiceRollsForNDice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrintPossibleDiceRollsForNDice
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      PrintDiceRolls(3);
+      Console.WriteLine("Hello World!");
+    }
+
+    static void PrintDiceRolls(int n)
+    {
+      List<int> diceRolls = new List<int>();
+      PrintDiceRollsHelper(n, diceRolls);
+    }
+
+    static void PrintDiceRollsHelper(int n, List<int> diceRolls)
+    {
+      if (n == 0)
+      {
+        Print(diceRolls);
+      }
+      else
+      {
+        for (int i = 1; i <= 6; i++)
+        {
+          diceRolls.Add(i);
+          PrintDiceRollsHelper(n - 1, diceRolls);
+          diceRolls.RemoveAt(diceRolls.Count - 1);
+        }      
+      }
+
+    }
+
+    private static void Print(List<int> diceRolls)
+    {
+      Console.Write(String.Join(",", diceRolls));
+      Console.WriteLine();
+    }
+  }
+}
