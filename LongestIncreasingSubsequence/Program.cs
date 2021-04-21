0a2ae130b2152827f080e5d3687bb7a560beead8
Karan Bajwa <v-bajkar@microsoft.com>
2021-04-20 18:05:07 -0700

    Added 1. Longest increasing subsequence 2. Maximu cuts

---
+++ LongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LongestIncreasingSubsequence
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] nums = { 3, 4, 2, 8, 10 };
+      Console.WriteLine(CountLongestIncreasingSubsequence(nums));
+
+      int[] nums1 = { 3, 4, 5, 8, 10,5,1 };
+      Console.WriteLine(CountLongestIncreasingSubsequence(nums1));
+
+      int[] nums2 = { 10, 5, 18, 7, 2, 9 };
+      Console.WriteLine(CountLongestIncreasingSubsequence(nums2));
+      Console.WriteLine("What a wonderful World!");
+    }
+
+    static int CountLongestIncreasingSubsequence(int[] nums)
+    {
+      if (nums == null)
+        throw new ArgumentNullException("Please provide valid input");
+
+      int[] dp = new int[nums.Length];
+      dp[0] = 1;
+
+      for (int i = 1; i < nums.Length; i++)
+      {
+        dp[i] = 1;  
+        for (int j = 0; j < i; j++)
+        {
+          if (nums[i] > nums[j]) // if there a re equal elements do we include that
+          {
+            dp[i] = Math.Max(dp[i], dp[j] + 1);
+          }
+        }
+      }
+      int max = 0;
+      for (int i=0; i < nums.Length; i++)
+      {
+        if (max < dp[i])
+          max = dp[i];
+      }
+      return max;
+    }
+  }
+}
---
+++ MaximumCuts/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MaximumCuts
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(MaximumCut(5, 1, 2, 3));
+      Console.WriteLine(MaximumCut(23, 12, 11, 13));
+      Console.WriteLine(MaximumCut(3, 2, 4, 2));
+
+      Console.WriteLine("What a wonderful World!");
+    }
+
+    static int MaximumCut(int n, int a, int b, int c)
+    {
+      int[] dp = new int[n+1];
+      dp[0] = 0;
+
+      for (int i = 1; i <= n; i++)
+      {
+        dp[i] = -1;
+        if (i - a >= 0)
+          dp[i] = Math.Max(dp[i], dp[i - a]);
+        if (i - b >= 0)
+          dp[i] = Math.Max(dp[i], dp[i - b]);
+        if (i - c >= 0)
+          dp[i] = Math.Max(dp[i], dp[i - c]);
+
+        if (dp[i] != -1)
+          dp[i]++;
+      }
+      return dp[n];    
+    }
+  }
+}
